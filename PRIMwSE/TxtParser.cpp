ca25647d203764a1be76c0366139e8f0706a386a
Commentsareuseless <55753818+Commentsareuseless@users.noreply.github.com>
2021-09-03 16:52:27 +0200

    unbreakabler

--- PRIMwSE/TxtParser.cpp
+++ PRIMwSE/TxtParser.cpp
@@ -13,7 +13,7 @@ int TxtParser::MakeMatrix(NeighMatrix& out_matrix, const FileName& file)
 	int matrixSize{ 0 };
 	int matrixElem{ 0 };
 	bool matrixFound{ false };
-	bool eomFound{ false };
+	bool eomFound{ false }; // end of matrix
 	
 	if (!file2Parse)
 	{
@@ -30,7 +30,7 @@ int TxtParser::MakeMatrix(NeighMatrix& out_matrix, const FileName& file)
 		numOfTokens = tokenizer.GetNumOfTokens();
 		do{
 			currentToken = tokenizer.NextToken();
-			// printf("%s ", currentToken.c_str()); // debug
+			 printf("%s ", currentToken.c_str()); // debug
 			if (currentToken[0] == '[')
 			{
 				matrixFound = true;
@@ -55,14 +55,14 @@ int TxtParser::MakeMatrix(NeighMatrix& out_matrix, const FileName& file)
 				}
 				catch (std::invalid_argument& arg)
 				{
-					(void)arg;
+					(void)arg;	// Skip NaN 
 					continue;
 				}
 
 				out_matrix.push_back(matrixElem);
 			}
 		} while (!currentToken.empty());
-		// printf("\n"); // debug
+		 printf("\n"); // debug
 	}
 	printf("Output vector: \n");
 	for (auto num : out_matrix)
@@ -99,7 +99,7 @@ void TxtParser::Tokenizer::Tokenize(const std::string& str, char delim)
 
 	for (; strIter != str.end(); strIter++)
 	{
-		if (*strIter == delim) 
+		if (*strIter == delim || *strIter == '\t')
 		{
 			continue;	// ignore additional delimiters
 		}
@@ -109,7 +109,9 @@ void TxtParser::Tokenizer::Tokenize(const std::string& str, char delim)
 		tmpStr.reserve(10);
 		try
 		{
-			while (strIter != str.end() && *strIter != delim)
+			while (strIter != str.end() &&
+				*strIter != delim &&
+				*strIter != '\t')
 			{
 				tmpStr.push_back(*strIter);
 				++strIter;
